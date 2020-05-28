2ce61b3b707bec9fc7f5b239f7d1a2705197e856
tanmx <tanmingxiao@gmail.com>
2020-05-28 13:55:26 +0800

    #添加pear

--- Script/pear.js
+++ Script/pear.js
@@ -2,24 +2,27 @@
 app下载地址：https://t.cn/A6htR2an
 
 #圈Xpear解锁会员
-^https:\/\/m\.pearkin\.com\/(api\/Movie\/WatchMovie|api\/Account\/CheckVip|api\/account\/IndexDetail) url script-response-body pear.js
+^https:\/\/ayk\.tmdidi\.com\/(api\/movie\/WatchMovie|api\/Account\/CheckVip|api\/account\/IndexDetail|api\/account\/UserScore) url script-response-body tanmx/Script/pear.js
 
-MITM = m.pearkin.com
-
+MITM = m.pearkin.com, ayk.tmdidi.com,
 */
 
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/api/Movie/WatchMovie';
+const vip = '/api/movie/WatchMovie';
 
 const checkvip = '/api/Account/CheckVip';
 
 const vipinfo = '/api/account/IndexDetail';
 
+const jf = '/api/account/UserScore';
+
 if (url.indexOf(vip) != -1) {
 	obj["canWath"] = "true";
+	obj["hadWach"] = "true";
+	obj["surplusCount"] = "999";
 	body = JSON.stringify(obj);
  }
 
@@ -29,11 +32,15 @@ if (url.indexOf(checkvip) != -1) {
 	body = JSON.stringify(obj);
  }
 if (url.indexOf(vipinfo) != -1) {
-	obj["nickName"] = "好心人";
-   obj["vipLevel"] = "101";
-   obj["vipEndTime"] = "2222-02-22";
+	obj["nickName"] = "解锁";
+   obj["vipLevel"] = "100";
+   obj["vipEndTime"] = "2099-11-11";
    obj["cartoonVip"] = "true";
 	body = JSON.stringify(obj);
  }
-$done({body});
+if (url.indexOf(jf) != -1) {
+	obj["value"] = "99999";
+	body = JSON.stringify(obj);
+ }
 
+$done({body});
